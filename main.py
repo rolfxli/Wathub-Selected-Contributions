2305eacc1296ce13c49dae1376e65a4d6aa1936f
Rolf Li <rolf.x.li@gmail.com>
2020-04-07 17:52:33 -0400

    scraping data

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 import os
 import json
 
+from selenium import webdriver
+#from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
+from typing import List, Dict
+
+BASE_URL = 'https://uwaterloo.ca/important-dates/important-dates/list'
+
+ACADEMIC_YEAR = 'Sep 1, 2019 to Aug 31, 2020'
+ACADEMIC_TERM = 'Spring'
+TERM_NUMBER = '1205'
+
+def scrape_dates(url: str, file_path: str):
+
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+def json_conversion(raw: str):
+
+
+def main():
+    result = str(scrape_dates(BASE_URL))
+
+if __name__ == '__main__':
+    main()
+
+
