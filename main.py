251e04274839d4f89487ea2b697fbec2011b0a36
Rolf Li <rolf.x.li@gmail.com>
2020-04-07 20:27:33 -0400

    adding json conversion

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import os
 import json
 import requests
 
-#from selenium import webdriver
-#from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 from typing import List, Dict
 
@@ -13,43 +11,86 @@ DIRECT_URL = 'https://uwaterloo.ca/important-dates/important-dates/list?date=Tod
 ALL_DATES_BASE_URL = 'https://uwaterloo.ca/important-dates/important-dates/list?academic_term=All&academic_year='
 ACADEMIC_YEAR_VALUE='214'
 ALL_DATES_PAGE='&audience=All&combine=&date=Any&type=All&=All&page='
+ALL_DATES='&audience=All&combine=&date=Any&type=All&=All'
 
-ACADEMIC_YEAR = 'Sep 1, 2019 to Aug 31, 2020'
-ACADEMIC_TERM = 'Spring'
-TERM_NUMBER = '1205'
+def load_pages(url: str, count: int, year: int):
+    #year_2018_2019 = 188
+    #year_2019_2020 = 214
+    #year_2020_2021 = 229
+    baseUrl = ALL_DATES_BASE_URL
+    academicYear = year
+    baseEndUrl = ALL_DATES
+    firstUrl = baseUrl + academicYear + baseEndUrl
+    redirectionUrl = "https://uwaterloo.ca"
 
-def load_pages(url: str, file_path: str):
-    year_2018_2019 = 188
-    year_2019_2020 = 214
-    year_2020_2021 = 229
+    important_dates = []
+    #print(count)
+    for page in range(count):
+        final_url = ""
+        if (page == 0):
+            final_url = firstUrl
+        else:
+            final_url = url + str(page)
+        #print(final_url)
 
-    #driver = webdriver.Chrome()
-    #driver.get(url)
+        page = requests.get(final_url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        #events = soup.find_all('tbody')
+        #print(events)
+        evens = soup.find_all(class_="even")
+        #print (evens[0])
+        count = 0
+        for even in evens:
+            hrefs = even.find_all('a')
+            title = even.find(class_="views-field views-field-title-1").text.strip()
+            titleHref = redirectionUrl + str(hrefs[0].get('href'))
+            description = even.find(class_="views-field views-field-field-uw-imp-dates-description").text.strip()
+            term = even.find(class_="views-field views-field-name-2").text.strip()
+            dates = even.find(class_="views-field views-field-field-uw-imp-dates-date active").text.strip()
 
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+            important_date = "{\"Title\": \"" + str(title) + "\", \"TitleRef\": \"" + str(titleHref) + "\", \"Description\": \"" + str(description) + "\", \"Term\": \"" + str(term) + "\", \"Dates\": \"" + str(dates) + "\"}" 
+            important_dates.append(important_date)
+            count += 1
+        #print(count)
+    
+    print(important_dates)
 
 def get_total_pages(year: str):
+    # Create the query url
     baseUrl = ALL_DATES_BASE_URL
     academicYear = year
     endUrl = ALL_DATES_PAGE
+    baseEndUrl = ALL_DATES
+    allUrl = baseUrl + academicYear + baseEndUrl
+    condensedUrl = baseUrl + academicYear + endUrl
     url = baseUrl + academicYear + endUrl + "0"
 
+
+    pageCount = 1
     page = requests.get(url)
-    #print(page.status_code)
+
+    # Ensure that the status of the page is 200-299 to indicate success
+    status = page.status_code
+    realStatus = int(status / 100)
+    if (realStatus != 2):
+        return
+
+    # Instantiate BeautifulSoup
     soup = BeautifulSoup(page.content, "html.parser")
 
-    soup.find_all(class_="pager")
-
-
+    foundPages = soup.find_all(class_="pager-item")
+    for result in foundPages:
+        #print(result)
+        pageCount += 1
+    
+    print('\n')
+    load_pages(condensedUrl, pageCount, year)
 
 #def json_conversion(raw: str):
 
 
 def main():
     get_total_pages(ACADEMIC_YEAR_VALUE)
-
-
 
 if __name__ == '__main__':
     main()
